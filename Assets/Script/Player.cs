c47ac36a6261e2b14a9fcb53194494fe957a8a3e
Victor Hugo P. A <68827639+Cataxis@users.noreply.github.com>
2023-07-12 10:44:33 -0600

    Version 2

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,15 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
-{   
+{
     [SerializeField] private float moveSpeed;
     [SerializeField] private float bounds = 4.5f;
+    private bool isPaused = false;
 
     void Update()
     {
-        Move();
+        if (!isPaused)
+        {
+            Move();
+        }
+
+        // Reiniciar escena si se presiona la tecla "R" o "Y"
+        if (Input.GetKeyDown(KeyCode.R) || Input.GetKeyDown(KeyCode.Joystick1Button3))
+        {
+            RestartScene();
+        }
+
+        // Pausar o reanudar juego si se presiona la tecla "P" o "Start" en el control de Xbox
+        if (Input.GetKeyDown(KeyCode.P) || Input.GetKeyDown(KeyCode.Joystick1Button7))
+        {
+            TogglePauseGame();
+        }
     }
 
     private void Move()
@@ -20,5 +37,16 @@ public class Player : MonoBehaviour
 
         playerPosition.x = Mathf.Clamp(playerPosition.x + moveInput * normalizedMoveSpeed, -bounds, bounds);
         transform.position = playerPosition;
+    }
+
+    private void RestartScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private void TogglePauseGame()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = isPaused ? 0f : 1f;
     }
 }
